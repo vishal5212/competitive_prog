457b469cd4928c8c31216447ab0d9e31438256e7
vishal5212 <44751530+vishal5212@users.noreply.github.com>
2019-07-05 10:23:11 +0530

    Update a.c

--- codeforces/div2/559/a.c
+++ codeforces/div2/559/a.c
@@ -1,3 +1,6 @@
+//was giving errors due to input which was taking space as the first char that's why the statement on line7 to waste out the 
+//space char
+
 #include<stdio.h>
 int main(){
     int n,a=0;scanf("%d",&n);
