e4819a27b0013002edca0c5472cc8f49d37efae2
vishal5212 <44751530+vishal5212@users.noreply.github.com>
2019-07-05 13:23:28 +0530

    Update tihonus.cpp

--- codeforces/div2/559/b/tihonus.cpp
+++ codeforces/div2/559/b/tihonus.cpp
@@ -9,6 +9,7 @@ using namespace std;
 #define INF 1000000007
  
 vector < pair <i64, int> > a;
+//vector of type pair and pair of 64-bit int and  a normal int
 int maxv[MAX], minv[MAX];
  
 int main() {
@@ -18,19 +19,33 @@ int main() {
 	int n; cin >> n;
 	int zero = 0;
 	for (int i = 1; i <= n; i++) {
+		//i64 seems to be some datatype. thsi is all i've been able to find out 
+		//https://stackoverflow.com/questions/25692848/error-invalid-suffix-i64-on-integer-constant
+		//you fucking idiot it has already been defined at the top
 		i64 x; cin >> x;
 		if (x == 0) zero++;
+		
+		//push elements as pairs of the element with the corresponding index
 		a.pb(mp(x, i));
 	}
-	i64 resK = INF;
+	i64 resK = INF;//you see how easy it is to define inf in c++
 	if (zero == 0) {
+		
+		//sorts the list but for what
 		sort(a.begin(), a.end());
-		maxv[n - 1] = a[n - 1].second;
+		
+		
+		//just initializing the maxv and minv that's why they are them same
+		maxv[n - 1] = a[n - 1].second;//index of the max or min elemnt
 		minv[n - 1] = a[n - 1].second;
+		
+		//maxv and minv are arrays of size n-1 and starting from end the max and min index of every parir is stored
 		for (int i = n - 2; i >= 0; i--) {
 			maxv[i] = max(a[i].second, maxv[i + 1]);
 			minv[i] = min(a[i].second, minv[i + 1]);
 		}
+		
+		
 		for (int i = 0; i < n - 1; i++) {
 			i64 minValue = a[i].first;
 			int deltaI = max(abs(a[i].second - maxv[i + 1]), abs(a[i].second - minv[i + 1]));
