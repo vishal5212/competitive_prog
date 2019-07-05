79e215429a7e1bd1270007bf4864fa0f6a2dd7f6
vishal5212 <44751530+vishal5212@users.noreply.github.com>
2019-07-05 14:03:18 +0530

    Create mine.c

---
+++ codeforces/div2/559/c/mine.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int n,m;scanf("%d%d",&n,&m);
+    int b[n],g[m];
+    
+    for(int i=0;i<n;i++)scanf("%d",&b[i]);
+    for(int i=0;i<m;i++)scanf("%d",&g[i]);
+    return 0;
+}
