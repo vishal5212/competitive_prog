ab85f322444ad88f0e260be02e7e2e3016eab68f
vishal5212 <44751530+vishal5212@users.noreply.github.com>
2019-07-07 17:14:53 +0530

    Update naina_gupta.cpp

--- codeforces/div3/560/d/naina_gupta.cpp
+++ codeforces/div3/560/d/naina_gupta.cpp
@@ -1,3 +1,10 @@
+/*SUMMARY
+long long ints  t,n,x,i,j,a[n]
+int f
+f is init to 0
+*/
+
+
 #include<bits/stdc++.h>
 #define ll long long
 #define pb push_back
@@ -11,20 +18,28 @@ int main()
     cin.tie(0);
     cout.tie(0);
     
+    //long long ints
     ll t,n,x,i,j;
+    //int f
     int f;
     cin>>t;
     while(t){
       t--;
       cin>>n;
       ll a[n];
+      // f has been init to zero. what is f used for
       f=0;
       for(i=0;i<n;i++){
         cin>>a[i];
       }
+        
+      
       sort(a,a+n);
       x=a[0]*a[n-1];
+      // what is j used for
       j=0;
+        
+        //i*i<=x which means  for all ints less than the sqrt(result)
       for(i=2;i*i<=x;i++){
         if(x%i==0){
           if(a[j]!=i || a[n-1-j]!=(x/i)){
